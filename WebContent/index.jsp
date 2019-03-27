7d443c9b6855f7f591017ace3ce26e431d1d13b8
Shruthi G <shruthi202122@gmail.com>
2019-03-27 10:55:21 +0530

    Seperated code

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,17 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
-<script type="text/javascript" src="validations.js"></script>
+<script type="text/javascript" src="view/validations.js"></script>
 <style>
 body{
 	background-color:"lightblue";
 }
+div{
+color:red;
+}
 </style>
 </head>
 <body>
-<%@ include file="header.jsp" %>
+<%@ include file="view/header.jsp" %>
+<div>
+<c:if test="${errmsg!=null}">
+<c:out value="${errmsg}"/>
+</c:if></div>
 <form action="ProductServlet" method="post" id="productManagementForm">
 ProductId:<br>
 <input type="text" name="pid" id="pid"/><br><span id="pidErr" style="display:none; color:red;">enter product id</span><br><br>
--- src/com/product/servlet/ProductServlet.java
+++ src/com/product/servlet/ProductServlet.java
@@ -1,21 +1,18 @@
 package com.product.servlet;
 
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.product.InsertProduct;
 import com.product.ProductDb;
 public class ProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out=response.getWriter();
-		response.setContentType("text/html");
 		String pid1=request.getParameter("pid");
 		int pid=Integer.parseInt(pid1);
 		System.out.println("pid= "+pid);
@@ -29,47 +26,21 @@ public class ProductServlet extends HttpServlet {
 		
 		if(pid1!=null && price1.trim()!="" && name.trim()!=""){
 			ProductDb db=new ProductDb();
-			boolean b =db.checkProductDetails(pid);
-			if(!b) {
-				Connection con=null;
-				try {
-					Class.forName("com.mysql.jdbc.Driver");
-				    con = DriverManager.getConnection("jdbc:mysql://localhost/shruthidb", "shruthi", "abc");
-					PreparedStatement ps = con.prepareStatement("insert into product values(?,?,?)");
-					ps.setInt(1,pid);
-					ps.setString(2,name);
-					ps.setDouble(3,price);
-					
-					int i = ps.executeUpdate();
-					if(i>0) {
-						System.out.println("inserted successfully"); 
-						request.setAttribute("pid",pid);
-						request.setAttribute("name",name);
-						request.setAttribute("price",price);
-						request.getRequestDispatcher("successmsg.jsp").include(request,response);
-					}
-					else {
-						System.out.println("not inserted");
-					}
-				}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-				finally {
-					if (con != null) {
-						try {
-							con.close();
-						} 
-						catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
+			boolean isProductExists =db.checkProductDetails(pid);
+			if(!isProductExists) {
+				InsertProduct insertProduct=new InsertProduct();
+				boolean insertion=insertProduct.insertProductDetails(pid,name,price);
+				if(insertion) {
+					request.setAttribute("pid",pid);
+					request.setAttribute("name",name);
+					request.setAttribute("price",price);
+					request.getRequestDispatcher("/view/successmsg.jsp").include(request,response);
 				}
 				
 			}
 			else {
-				out.println("Trying to duplicate insertion");
-				request.getRequestDispatcher("index.jsp").include(request,response);
+				request.setAttribute("errmsg","Trying to insert duplicate values");
+				request.getRequestDispatcher("/index.jsp").include(request,response);
 			}
 		}
 		}
